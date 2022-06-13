2aba313df6b43c6e46a7784ba1b3b42f111fadc4
valdenio458 <valdeniomartinho@gmail.com>
2022-06-13 19:09:28 -0300

    desafio 02

---
+++ scripts-python/desafio-02.py
@@ -0,0 +1,4 @@
+dia = input('Qual o dia do seu nascimento?')
+mes = input('Qual o mes de seu nascimento?')
+ano = input('Qual o ano de seu nascimento?')
+print('Você nasceu no dia' , dia, 'de' ,mes ,'de', ano,'. Correto?')
