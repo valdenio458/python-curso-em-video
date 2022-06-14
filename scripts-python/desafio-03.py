22de76a51711dcb27f9914a81d0849c65fcf1937
valdenio458 <valdeniomartinho@gmail.com>
2022-06-14 11:45:26 -0300

    desafio-03

--- scripts-python/desafio-03.py
+++ scripts-python/desafio-03.py
@@ -1,3 +1,5 @@
-n1 = input('Digte um número:')
-n2 = input('Digite outro número:')
-print(sum('n1' , 'n2'))
+# Ler dois números e calcular a soma entre eles
+n1 = int(input('Digte um número:'))
+n2 = int(input('Digite outro número:'))
+s = n1 + n2
+print('A soma entre {} e {} é {}'.format(n1, n2, s))
