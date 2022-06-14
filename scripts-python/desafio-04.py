6e027177272855a691aec4426ce0106d9388daf6
valdenio458 <valdeniomartinho@gmail.com>
2022-06-14 11:45:35 -0300

    desafio-04

---
+++ scripts-python/desafio-04.py
@@ -0,0 +1,9 @@
+#Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e toas as informçãoes possíveis sobre ele.
+n = input('Digite algo: ')
+print('O tipo primitivo desse valor é: ', type(n))
+print('Só tem espaços? ', n.isspace())
+print('É um número? ', n.isnumeric())
+print('É alfabético? ', n.isalpha())
+print('É alfanumérico? ', n.isalnum())
+print('Está em maiúsculas? ', n.isupper())
+print('Está em minúsculas? ', n.islower())
