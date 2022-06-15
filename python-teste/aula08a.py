bf2e1ef86c80718d00d92212f84ccff1ae5bab04
valdenio458 <valdeniomartinho@gmail.com>
2022-06-15 17:06:21 -0300

    Aula 08a

---
+++ python-teste/aula08a.py
@@ -0,0 +1,14 @@
+#import math
+#num = int(input('Digite um número: '))
+#raiz = math.sqrt(num)
+#print('A raiz quadrada de {} é {:.2f}'.format(num, math.ceil(raiz)))
+
+
+#from math import sqrt, ceil
+#num = int(input('Digite um número: '))
+#raiz = sqrt(num)
+#print('A raiz quadrada de {} é {:.2f}'.format(num, ceil(raiz)))
+
+import random
+num = random.randint(0, 10)
+print('O número sorteado foi: {}'.format(num))
