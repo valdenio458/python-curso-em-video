e05e571d446508fd3c8cf820fa3e9e371dfba1b0
valdenio458 <valdeniomartinho@gmail.com>
2022-06-25 17:50:35 -0300

    desafio-24

---
+++ scripts-python/desafio-24.py
@@ -0,0 +1,3 @@
+# Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO"
+cidade = str(input('Digite o nome da sua cidade: ')).strip()  
+print(cidade[:5].upper() == 'SANTO')
