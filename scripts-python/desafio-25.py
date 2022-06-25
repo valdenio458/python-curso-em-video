065897f08d3064170de10492305c249b67be2e1d
valdenio458 <valdeniomartinho@gmail.com>
2022-06-25 17:58:21 -0300

    desafio-25

---
+++ scripts-python/desafio-25.py
@@ -0,0 +1,3 @@
+# Crie um programa que leia o nome de uma pessoa e diga se ela tem 'SILVA' no nome.
+nome = str(input('Digite o seu nome: ')).strip()
+print('Seu nome tem Silva? {}'.format('SILVA' in nome.upper())) # imprime True se o texto SILVA estiver na frase
