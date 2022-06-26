ff34365dcfa3f7aad75043f264147e568f451338
valdenio458 <valdeniomartinho@gmail.com>
2022-06-26 10:33:08 -0300

    :recycle:refactor

--- scripts-python/desafio-28.py
+++ scripts-python/desafio-28.py
@@ -1,8 +1,14 @@
 # Escreva um programa qwue faça o computador "pensar" em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.
 import random
-n = random.randint(0, 5)
-num =int(input('Digite um número inteiro entre 0 e 5: '))
-if num == n:
+from time import sleep
+computador = random.randint(0, 5)
+print('-=-' * 20)
+print('Pensei em um número entre 0 e 5. Tente adivinhar qual foi: ')
+print('-=-' * 20)
+jogador =int(input('Em qual número eu pensei?: '))
+print('PROCESSANDO...')
+sleep(3)
+if jogador == computador:
     print('Você venceu!')
 else:
-    print('Você perdeu!')
+    print('Você perdeu! Eu pensei no número {} e não no {}'.format(computador, jogador))
