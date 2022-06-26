265c2eebbd69404fd7d5235fed9339e2f1006477
valdenio458 <valdeniomartinho@gmail.com>
2022-06-26 09:54:46 -0300

    desafio-30

---
+++ scripts-python/desafio-30.py
@@ -0,0 +1,6 @@
+# Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou IMPAR.
+num = int(input('Digite um número inteiro: '))
+if num % 2 == 0:
+    print('O número é PAR!')
+else:
+    print('O número é IMPAR!')
