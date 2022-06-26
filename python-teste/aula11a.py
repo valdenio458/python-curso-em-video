f46c226f8705fb77f2d77cf79021229f48539535
valdenio458 <valdeniomartinho@gmail.com>
2022-06-26 16:38:40 -0300

    Aula 11a

---
+++ python-teste/aula11a.py
@@ -0,0 +1,12 @@
+#print('\033[1;31;43mOlá, mundo!\033[m')
+#print('\033[7;37mOlá, mundo!\033[m')
+'''a = 3
+b = 5
+print('Os valores são \033[32m{}\033[m e \033[31m{}\033[m'.format(a, b))'''
+nome = 'Raphaela'
+cores = {'limpa': '\033[m',
+        'azul': '\033[34m',
+        'amarelo': '\033[33m',
+        'pretoebranco': '\033[7;30m',
+  }
+print('Olá! Muito prazer em te conhecer, {}{}{}!'.format(cores['amarelo'], nome,cores['limpa']))
