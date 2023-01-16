0b6860af205471f4fc66877d5df82e529a288a83
valdenio458 <valdeniomartinho@gmail.com>
2023-01-16 16:51:15 -0300

    Aula 13

---
+++ python-teste/aula13.py
@@ -0,0 +1,19 @@
+# for c in range(0, 7, 2):
+#     print(c)
+# print('FIM')
+
+# n = int(input('Digite um número: '))
+# for c in range(0, n + 1):
+#     print(c)
+
+# i = int(input('Início: '))
+# f = int(input('Fim: '))
+# p = int(input('Passo: '))
+# for c in range(i, f + p, p): 
+#     print(c)
+
+s = 0
+for c in range(0, 3): 
+  n = int(input('Digite um número: '))
+  s += n
+print(f'A soma dos números é {s}.')
