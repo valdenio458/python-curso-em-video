9495d0e2f3090caa68194142a547b6fa806aec1c
valdenio458 <valdeniomartinho@gmail.com>
2023-01-17 15:38:40 -0300

    Aula 14

---
+++ python-teste/aula14.py
@@ -0,0 +1,27 @@
+'''for c in range(1, 10): 
+    print(c)
+print('FIM')'''
+
+# c = 1
+# while c < 10: 
+#     print(c)
+#     c += 1 
+# print('FIM')
+
+# n = 1
+# r = 'S'
+# while r == 'S': # Flag, condição de parada
+#   n = int(input('Digite um valor: '))
+#   r = str(input('Quer continuar? [S/N]')).upper()
+# print('Fim')
+
+n = 1
+par = impar = 0
+while n != 0: 
+  n = int(input('Digite um valor: '))
+  if n != 0:
+    if n % 2 == 0: 
+      par += 1
+    else: 
+      impar += 1
+print(f'Você digitou {par} números pares e {impar} números ímpares.')
