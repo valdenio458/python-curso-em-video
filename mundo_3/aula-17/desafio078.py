3a3ec8b6e829ce7d4abe7ba6b971c489428842c7
valdenio458 <valdeniomartinho@gmail.com>
2023-09-20 20:07:51 -0300

    desafio 078

---
+++ mundo_3/aula-17/desafio078.py
@@ -0,0 +1,11 @@
+"""  Faça um programa que leia 5 valores numéricos e gurade-os em uma lista
+    No final, mostre qual foi o maior e menor valor digitado e as suas
+    respectivas posições na lista
+"""
+valores = []
+
+for p, c in enumerate(range(0, 5)):
+    valores.append(int(input('Digite um valor: ')))
+
+print(f'Maior valor {max(valores)} na posição: {valores.index(max(valores))}')
+print(f'Menor valor {min(valores)} na posição: {valores.index(min(valores))}')
