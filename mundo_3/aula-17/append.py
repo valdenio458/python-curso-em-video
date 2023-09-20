88f928be2f187644782a6739053cec3a21348eaa
valdenio458 <valdeniomartinho@gmail.com>
2023-09-20 18:27:28 -0300

    append

--- mundo_3/aula-17/append.py
+++ mundo_3/aula-17/append.py
@@ -1,3 +1,10 @@
-num = [2, 5, 9, 1]
-num.append(7)
-print(num)
+num = []
+# num = [2, 5, 9, 1]
+# num.append(7)
+# print(num)
+
+for c in range(0, 5):
+    num.append(int(input('Digite um valor :')))
+
+for c, v in enumerate(num):
+    print(f'Na posição {c} encontrei o valor {v}.')
