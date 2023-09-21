da5b9a03e639dbe92568801efaca7dbefdff4d61
valdenio458 <valdeniomartinho@gmail.com>
2023-09-21 15:14:46 -0300

    desafio 078

--- mundo_3/scripts/desafio078.py
+++ mundo_3/scripts/desafio078.py
@@ -7,5 +7,7 @@ valores = []
 for p, c in enumerate(range(0, 5)):
     valores.append(int(input('Digite um valor: ')))
 
+print('-=' * 30)
+print(f'Você digitou os valores {valores}')
 print(f'Maior valor {max(valores)} na posição: {valores.index(max(valores))}')
 print(f'Menor valor {min(valores)} na posição: {valores.index(min(valores))}')
