c29adf2f84355c1236be4f270b48fa8e57fface3
valdenio458 <valdeniomartinho@gmail.com>
2023-09-25 18:20:15 -0300

    desafio 090 refatorado

--- mundo_3/scripts/desafio090.py
+++ mundo_3/scripts/desafio090.py
@@ -5,11 +5,11 @@ Ao final mostre o conteúdo na tela.
 """
 aluno = dict()
 aluno['nome'] = str(input('Nome: '))
-aluno['media'] = float(input('Média: '))
+aluno['media'] = float(input(f'Média de {aluno["nome"]}: '))
 if aluno['media'] >= 7:
     aluno['situação'] = 'Aprovado(a)!'
 else:
     aluno['situação'] = 'Reprovado(a)'
-print(f'Nome é igual a {aluno["nome"]}')
-print(f'Média é igual a {aluno["media"]}')
-print(f'Situação é igual a {aluno["situação"]}')
+print('-=' * 30)
+for k, v in aluno.items():
+    print(f'  - {k} é igual a {v}')
