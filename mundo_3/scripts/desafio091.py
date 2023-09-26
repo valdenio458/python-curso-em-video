67e2d2580f6b7ef6fed84ff2752a3636e429bf6a
valdenio458 <valdeniomartinho@gmail.com>
2023-09-26 10:52:48 -0300

    desafio 091 - corrige identação

--- mundo_3/scripts/desafio091.py
+++ mundo_3/scripts/desafio091.py
@@ -9,10 +9,10 @@ from random import randint
 from time import sleep
 from operator import itemgetter
 jogo = {
-      'jogador1': randint(1, 6),
-      'jogador2': randint(1, 6),
-      'jogador3': randint(1, 6),
-      'jogador4': randint(1, 6)
+    'jogador1': randint(1, 6),
+    'jogador2': randint(1, 6),
+    'jogador3': randint(1, 6),
+    'jogador4': randint(1, 6)
 }
 ranking = list()
 print('Valores sorteados:')
