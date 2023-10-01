d41d49b47c20bf4c35902a9c5ae2a6add1d0a94b
valdenio458 <valdeniomartinho@gmail.com>
2023-10-01 14:58:08 -0300

    desafio 0103 - concluído

--- mundo_3/scripts/desafio0103.py.py
+++ mundo_3/scripts/desafio0103.py.py
@@ -5,5 +5,17 @@ mostrar a ficha do jogador, mesmo que algum dado não tenha
 sido informado corretamente."""
 
 
-def ficha():
-    pass
+def ficha(nome="<desconhecido>", gols=0):
+    print(f"O jogador {nome} fez {gols} gol(s). ")
+
+
+n = str(input("Nome do jogador: "))
+g = str(input("Número de gols: "))
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+if n.strip() == "":
+    ficha(gols=g)
+else:
+    ficha(n, g)
