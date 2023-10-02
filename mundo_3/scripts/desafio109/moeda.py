8a78f377d1b237d5644fd079478c4eef70475c24
valdenio458 <valdeniomartinho@gmail.com>
2023-10-02 17:23:38 -0300

    desafio 0109

---
+++ mundo_3/scripts/desafio109/moeda.py
@@ -0,0 +1,22 @@
+def metade(preco=0, format=False):
+    res = preco / 2
+    return res if format is False else moeda(res)
+
+
+def dobro(preco=0, format=False):
+    res = preco * 2
+    return res if format is False else moeda(res)
+
+
+def aumentar(preco=0, taxa=0, format=False):
+    res = preco + (preco * taxa / 100)
+    return res if not format else moeda(res)
+
+
+def diminuir(preco=0, taxa=0, format=False):
+    res = preco - (preco * taxa / 100)
+    return res if not format else moeda(res)
+
+
+def moeda(preco=0, moeda="R$"):
+    return f"{moeda}{preco:>.2f}".replace(".", ",")
