9bedf65485b2d3efc46b95e8e5f2a3bb221a5cd1
valdenio458 <valdeniomartinho@gmail.com>
2023-10-03 15:35:43 -0300

    desafio 0113

---
+++ mundo_3/scripts/desafio113/desafio113.py
@@ -0,0 +1,36 @@
+"""Reescreva a função leiaInt() que fizemos no desafio 104,
+incluindo agora a possibilidade da digitação de um número
+de tipo inválido. Aproveite e crie também uma função
+leiaFloat() com a mesma funcionalidade.
+"""
+
+
+def leiaInt(msg):
+    while True:
+        try:
+            n1 = int(input(msg))
+        except (ValueError, TypeError):
+            print("\033[31mERRO!Digite um número inteiro válido!\033[m")
+            continue
+        except KeyboardInterrupt:
+            print("\n\033[31mUsuário preferiru não digitar o número!\033[m")
+            return 0
+        else:
+            return n1
+
+
+def leiaFloat(msg):
+    while True:
+        try:
+            n2 = float(input(msg))
+        except (ValueError, TypeError):
+            print("\033[31mERRO!Digite um número inteiro válido!\033[m")
+            continue
+        except KeyboardInterrupt:
+            print("\n\033[31mUsuário preferiru não digitar o número!\033[m")
+            return 0
+        else:
+            return n2
+
+
+
---
+++ mundo_3/scripts/desafio113/read.py
@@ -0,0 +1,6 @@
+from desafio113 import leiaInt, leiaFloat
+
+
+n1 = leiaInt("Digite um valor inteiro: ")
+n2 = leiaFloat("Digite um valor real: ")
+print(f"O valor inteiro digitado foi {n1} e o real foi {n2}")
